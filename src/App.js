94e8f6038f32855b90440dd2f847afe9db7b4885
Sadera Nye <saderanye1@gmail.com>
2025-05-29 13:57:20 -0700

    turning website into a react app

--- src/App.js
+++ src/App.js
@@ -4,20 +4,41 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      {/* Header with navigation */}
+      <header>
+        <nav>
+          <ul>
+            <li><a href='#home'>Home</a></li>
+            <li><a href='#about'>About</a></li>
+            <li><a href='#shop'>Shop</a></li>
+            <li><a href='#contact'>Contact</a></li>
+          </ul>
+        </nav>
       </header>
+
+      {/* Sections */}
+      <section id='home'>
+        <h1>Welcome to Honey Junkies</h1>
+        <p>Your sweet spot for honey and more!</p>
+      </section>
+
+      <section id='about'>
+        <h1>About Honey Junkies</h1>
+        <p>Here's where you can tell visitors about your business</p>
+      </section>
+
+      <section id='gallery'>
+        <h1>Gallery</h1>
+        {/* gallery will go here */}
+      </section>
+
+      <section id='contact'>
+        <h1>Contact Us</h1>
+        {/* contact form goes here */}
+      </section>
+
+
     </div>
   );
 }
